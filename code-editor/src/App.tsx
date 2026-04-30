8c8ce01d3b0d68f9d48691cae83190d8809bf7b0
SpectroXRL <spectrothedev@gmail.com>
2026-04-30 12:00:29 -0500

    feat: remove unused routes and toast container
    - Remove unused imports and route definitions for SubjectPage and ChallengePage components. 
    - Remove ToastContainer component from the app layout as it is no longer needed. 
    - Simplify the routing structure to only include HomePage and LearnPage routes.

--- code-editor/src/App.tsx
+++ code-editor/src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { ToastContainer } from "./components/Toast";
 import { HomePage } from "./pages/HomePage";
-import { SubjectPage } from "./pages/SubjectPage";
-import { ChallengePage } from "./pages/ChallengePage";
 import { LearnPage } from "./pages/LearnPage";
 import "./App.css";
 
@@ -14,15 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/learn" element={<LearnPage />} />
-          <Route path="/subjects/:subjectId" element={<SubjectPage />} />
-          <Route path="/challenge/:challengeId" element={<ChallengePage />} />
-          <Route path="/challenge/topic/:topicId" element={<ChallengePage />} />
-          <Route
-            path="/challenge/subject/:subjectId"
-            element={<ChallengePage />}
-          />
         </Routes>
-        <ToastContainer />
       </AuthProvider>
     </BrowserRouter>
   );
